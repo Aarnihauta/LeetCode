984c9c1f45667b1d1a75d5c536dff63bebc1eb44
Nikita Bukov <n.bukov@appricot.com>
2023-02-23 01:43:26 +0300

    Add project files.

---
+++ TrueLeetCode/Leetcode/L371.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Emit;
+
+namespace TrueLeetCode.Leetcode;
+public class L371
+{
+    public int TwoSum(int a, int b)
+    {
+        DynamicMethod method = new DynamicMethod("leetcode_371", typeof(int), new Type[] { typeof(int), typeof(int) });
+        var generator = method.GetILGenerator();
+        generator.Emit(OpCodes.Ldarg_0);
+        generator.Emit(OpCodes.Ldarg_1);
+        generator.Emit(OpCodes.Add);
+        generator.Emit(OpCodes.Ret);
+
+        return (int)method.Invoke(null, new object[] { a, b });
+    }
+}
