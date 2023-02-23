f8ad8c12d213f55f99df3e01fef50dcb0cfebf18
Nikita Bukov <n.bukov@appricot.com>
2023-02-23 13:48:58 +0300

    WIP

--- TrueLeetCode/Leetcode/L231.cs
+++ TrueLeetCode/Leetcode/L231.cs
@@ -6,28 +6,45 @@ public class L231
     //https://leetcode.com/problems/power-of-two/
     public static bool IsPowerOfTwo(int n)
     {
+        var mathAbs = typeof(Math).GetMethod("Abs", new Type[] { typeof(int) });
+
         DynamicMethod method = new DynamicMethod("leetcode_231", typeof(bool), new Type[] { typeof(int) });
-        
         var generator = method.GetILGenerator();
-                                                        //#description                                              #stack history
 
         var start = generator.DefineLabel();
-        generator.Emit(OpCodes.Ldarg_0);                //load 'n' at index 0 onto the stack                        n
-        generator.Emit(OpCodes.Brtrue, start);          //if n != 0 go to 'start'                                   
-        generator.Emit(OpCodes.Ldc_I4_0);               //push 0 value onto the stack (int32)                       0, n
-        generator.Emit(OpCodes.Ret);                    //return 0 (false)
+
+        generator.Emit(OpCodes.Ldarg_0);
+        generator.Emit(OpCodes.Ldc_I4_1);
+        generator.Emit(OpCodes.Ble_S, start);
+
+        generator.Emit(OpCodes.Ldarg_0);
+        generator.Emit(OpCodes.Ldarg_0);
+        generator.Emit(OpCodes.Ldc_I4_1);
+        generator.Emit(OpCodes.Sub);
+        generator.Emit(OpCodes.And);
+        generator.Emit(OpCodes.Ldc_I4_0);
+        generator.Emit(OpCodes.Ceq);
+        generator.Emit(OpCodes.Ret);
 
         generator.MarkLabel(start);
-        generator.Emit(OpCodes.Ldarg_0);                //load 'n' at index 0 onto the stack                        n
-        generator.Emit(OpCodes.Ldarg_0);                //load 'n' at index 0 onto the stack                        n, n
-        generator.Emit(OpCodes.Ldc_I4_1);               //load '1' int value onto the stack (0 index)               1, n, n
-        generator.Emit(OpCodes.Sub);                    //n - 1, result pushed onto the stack (0 index)             x = n - 1, n
-        generator.Emit(OpCodes.And);                    //bitwise AND of two values and pushes onto the stack       y = x & n
-        generator.Emit(OpCodes.Ldc_I4_0);               //push 0 value onto the stack (int32)                       0, y
-        generator.Emit(OpCodes.Ceq);                    //compares two values and pushed 0 or 1 onto the stack      c = 0 == y
-        generator.Emit(OpCodes.Ret);                    //return 'c'
-
+        generator.Emit(OpCodes.Ldc_I4_0);
+        generator.Emit(OpCodes.Ret);
 
         return (bool)method.Invoke(null, new object[] { n });
+    }
+
+
+    public static int MathAbs(int n)
+    {
+        DynamicMethod method = new DynamicMethod("leetcode_231", typeof(int), new Type[] { typeof(int) });
+        var generator = method.GetILGenerator();
+
+        var mathAbs = typeof(Math).GetMethod("Abs", new Type[] { typeof(int) });
+
+        generator.Emit(OpCodes.Ldarg_0);
+        generator.Emit(OpCodes.Call, mathAbs);
+        generator.Emit(OpCodes.Ret);
+
+        return (int)method.Invoke(null, new object[] { n });
     }
 }
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,8 +1,23 @@
 ﻿using TrueLeetCode.Leetcode;
 
-Console.WriteLine((0 & (0 - 1)));
-Console.WriteLine((0 & (0 - 1))== 0);
+var a = L231.IsPowerOfTwo(0);
+var b = L231.IsPowerOfTwo(3);
+var c = L231.IsPowerOfTwo(32);
+var d = L231.IsPowerOfTwo(-16);
+var f = L231.IsPowerOfTwo(1);
 
-var c = L231.IsPowerOfTwo(0);
 
+Console.WriteLine(a);
+Console.WriteLine(b);
 Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(f);
+
+
+var q = M(1);
+Console.WriteLine(q);
+
+static bool M(int n)
+{
+    return n > 0 && (n & (n - 1)) == 0;
+}
