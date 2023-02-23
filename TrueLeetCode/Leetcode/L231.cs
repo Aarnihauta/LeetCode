9fcea8ed48b26989909745b3547f67d9fd08cffb
Nikita Bukov <n.bukov@appricot.com>
2023-02-23 14:05:19 +0300

    Complete L231

--- TrueLeetCode/Leetcode/L231.cs
+++ TrueLeetCode/Leetcode/L231.cs
@@ -6,15 +6,13 @@ public class L231
     //https://leetcode.com/problems/power-of-two/
     public static bool IsPowerOfTwo(int n)
     {
-        var mathAbs = typeof(Math).GetMethod("Abs", new Type[] { typeof(int) });
-
         DynamicMethod method = new DynamicMethod("leetcode_231", typeof(bool), new Type[] { typeof(int) });
         var generator = method.GetILGenerator();
 
         var start = generator.DefineLabel();
 
         generator.Emit(OpCodes.Ldarg_0);
-        generator.Emit(OpCodes.Ldc_I4_1);
+        generator.Emit(OpCodes.Ldc_I4_0);
         generator.Emit(OpCodes.Ble_S, start);
 
         generator.Emit(OpCodes.Ldarg_0);
@@ -31,20 +29,5 @@ public class L231
         generator.Emit(OpCodes.Ret);
 
         return (bool)method.Invoke(null, new object[] { n });
-    }
-
-
-    public static int MathAbs(int n)
-    {
-        DynamicMethod method = new DynamicMethod("leetcode_231", typeof(int), new Type[] { typeof(int) });
-        var generator = method.GetILGenerator();
-
-        var mathAbs = typeof(Math).GetMethod("Abs", new Type[] { typeof(int) });
-
-        generator.Emit(OpCodes.Ldarg_0);
-        generator.Emit(OpCodes.Call, mathAbs);
-        generator.Emit(OpCodes.Ret);
-
-        return (int)method.Invoke(null, new object[] { n });
     }
 }
