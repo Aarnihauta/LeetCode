b40741e1929c14ce91538afa06ecc50c8bb347e5
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-02-25 10:34:07 +0300

    complete L1290 (#3)

---
+++ TrueLeetCode/Leetcode/LinkedList/L1290.cs
@@ -0,0 +1,16 @@
+﻿namespace TrueLeetCode.Leetcode;
+
+//https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+public class L1290
+{
+    public int GetDecimalValue(ListNode head)
+    {
+        int result = 0;
+        while (head != null)
+        {
+            result = result << 1 | head.val;
+            head = head.next;
+        }
+        return result;
+    }
+}
