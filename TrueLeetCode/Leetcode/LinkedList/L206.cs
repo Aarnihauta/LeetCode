374da676aa5943712cc709252d1b05396dd82056
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-02-25 12:45:17 +0300

    complete L206 (#5)

---
+++ TrueLeetCode/Leetcode/LinkedList/L206.cs
@@ -0,0 +1,19 @@
+﻿namespace TrueLeetCode.Leetcode;
+
+//https://leetcode.com/problems/reverse-linked-list/
+public class L206
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        var current = head;
+        ListNode previous = null;
+        while(current != null)
+        {
+            ListNode? next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return head = previous;
+    }
+}
