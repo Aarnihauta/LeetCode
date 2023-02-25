240e7e6ce713800ea6f947e1c3826be5c78e603a
Nikita Bukov <n.bukov@appricot.com>
2023-02-25 11:45:10 +0300

    complete l876

---
+++ TrueLeetCode/Leetcode/LinkedList/L876.cs
@@ -0,0 +1,16 @@
+﻿namespace TrueLeetCode.Leetcode;
+public class L876
+{
+    //https://leetcode.com/problems/middle-of-the-linked-list
+    public ListNode MiddleNode(ListNode head)
+    {
+        List<ListNode> list = new List<ListNode>();
+
+        while(head != null)
+        {
+            list.Add(head);
+            head = head.next;
+        }
+        return list[list.Count / 2];
+    }
+}
