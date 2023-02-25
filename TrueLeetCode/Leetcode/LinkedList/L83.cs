8adb21f2b77a23d27931c9db2dab7ee28c554ac7
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-02-25 22:43:33 +0300

    complete L83 (#6)

---
+++ TrueLeetCode/Leetcode/LinkedList/L83.cs
@@ -0,0 +1,23 @@
+﻿namespace TrueLeetCode.Leetcode;
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+public class L83
+{
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        var result = head;
+
+        while (head != null && head.next != null)
+        {
+            if (head.val == head.next.val)
+            {
+                head.next = head.next.next;
+                continue;
+            }
+
+            head = head.next;
+        }
+
+        return result;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using TrueLeetCode.Leetcode;
+
+var l = new L83();
+
+var q = l.DeleteDuplicates(new(1, new(1, new(2, new(3, new(3, new(4)))))));
