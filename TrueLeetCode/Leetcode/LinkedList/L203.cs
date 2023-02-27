595934c0d6c526764749ccccb2115518acbb8a05
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-02-27 22:54:01 +0300

    complete L203 (#7)

---
+++ TrueLeetCode/Leetcode/LinkedList/L203.cs
@@ -0,0 +1,27 @@
+﻿namespace TrueLeetCode.Leetcode;
+
+//https://leetcode.com/problems/remove-linked-list-elements/
+public class L203
+{
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+        while(head != null && head.val == val)
+        {
+            head = head.next;
+        }
+
+        var result = head;
+        while (head != null)
+        {
+            if (head.next?.val == val)
+            {
+                head.next = head.next.next;
+            }
+            else
+            {
+                head = head.next;
+            }
+        }
+        return result;
+    }
+}
