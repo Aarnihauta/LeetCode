0ccc904dc715a858ecd97f6a26496fa454aedf5d
Nikita Bukov <n.bukov@appricot.com>
2023-05-01 16:26:43 +0300

    Complete L2325

---
+++ TrueLeetCode/Leetcode/HashTable/L2325.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+//https://leetcode.com/problems/decode-the-message/
+public class L2325
+{
+    public string DecodeMessage(string key, string message)
+    {
+        var result = new StringBuilder();
+        var alphabet = new Dictionary<char, char>();
+       
+        var currentLetter = 'a';
+        foreach(var c in key)
+        {
+            if(char.IsLetter(c) && !alphabet.ContainsKey(c))
+            {
+                alphabet[c] = currentLetter;
+                currentLetter = (char)(currentLetter + 1);
+            }
+            
+        }
+
+        foreach(var c in message)
+        {
+            if(char.IsLetter(c))
+            {
+                result.Append(alphabet[c]);
+            }
+            else
+            {
+                result.Append(' ');
+            }
+        }
+
+        return result.ToString();
+    }
+}
