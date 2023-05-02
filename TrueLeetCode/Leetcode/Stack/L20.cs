7aa5d093a52e46dde4a40fdcfa92db650504bec9
Nikita Bukov <n.bukov@appricot.com>
2023-05-02 10:35:19 +0300

    Complete L20

---
+++ TrueLeetCode/Leetcode/Stack/L20.cs
@@ -0,0 +1,37 @@
+﻿namespace TrueLeetCode.Leetcode.Stack;
+
+//https://leetcode.com/problems/valid-parentheses/
+public class L20
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        foreach (var c in s)
+        {
+            if(c == '(' || c == '[' || c == '{')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if(stack.Count == 0)
+                {
+                    return false;
+                }
+
+                if ((c == ')' && stack.Peek() == '(') ||
+                    (c == ']' && stack.Peek() == '[') ||
+                    (c == '}' && stack.Peek() == '{'))
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,7 +1,1 @@
-﻿using TrueLeetCode.Leetcode.HashTable;
-
-var l = new L1832();
-
-var r = l.CheckIfPangram("thequickbrownfoxjumpsoverthelazydog");
-
-Console.WriteLine(r);
+﻿
