c4256e5ad0c022a9dafd89dacbc84d73d2d990fd
Nikita Bukov <n.bukov@appricot.com>
2023-05-02 22:47:48 +0300

    Complete L1614

---
+++ TrueLeetCode/Leetcode/Stack/L1614.cs
@@ -0,0 +1,24 @@
+﻿namespace TrueLeetCode.Leetcode.Stack;
+
+//https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses
+public class L1614
+{
+    public int MaxDepth(string s)
+    {
+        int currentMax = 0;
+        int result = 0;
+        foreach (var c in s)
+        {
+            if (c == '(')
+            {
+                currentMax++;
+            }
+            else if (c == ')')
+            {
+                result = Math.Max(result, currentMax--);
+            }
+        }
+
+        return result;
+    }
+}
