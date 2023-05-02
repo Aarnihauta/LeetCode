cf88b043667ceeb763c9dcd07317d38bed6cecdb
Nikita Bukov <n.bukov@appricot.com>
2023-05-02 23:43:25 +0300

    Complete L589

---
+++ TrueLeetCode/Leetcode/Trees/L589.cs
@@ -0,0 +1,25 @@
+﻿namespace TrueLeetCode.Leetcode.Trees;
+public class L589
+{
+    public IList<int> Preorder(Node root)
+    {
+        var list = new List<int>();
+        PreorderTraversal(root, list);
+        return list;
+    }
+
+    private void PreorderTraversal(Node root, List<int> list)
+    {
+        if (root != null)
+        {
+            list.Add(root.val);
+            if(root.children != null)
+            {
+                foreach (var child in root.children)
+                {
+                    PreorderTraversal(child, list);
+                }
+            }
+        }
+    }
+}
