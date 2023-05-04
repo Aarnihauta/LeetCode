5438a999c8c781e755aa18045bae947f64ca8fe0
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-05-04 23:04:50 +0300

    L897 (#16)
    * Complete L897
    
    * update net6 to net7
    
    ---------
    
    Co-authored-by: Nikita Bukov <n.bukov@appricot.com>

---
+++ TrueLeetCode/Leetcode/Trees/L897.cs
@@ -0,0 +1,44 @@
+﻿namespace TrueLeetCode.Leetcode.Trees;
+public class L897
+{
+    public TreeNode IncreasingBST(TreeNode root)
+    {
+        var set = new HashSet<int>();
+        Inorder(root, set);
+
+        set = set.OrderBy(x => x).ToHashSet();
+         
+        var node = new TreeNode();
+        CreateTreeNode(node, set);
+        return node;
+    }
+
+    private void Inorder(TreeNode root, HashSet<int> set)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        if(!set.Contains(root.val))
+        {
+            set.Add(root.val);
+        }
+        Inorder(root.left, set);
+        Inorder(root.right, set);
+    }
+
+    private TreeNode CreateTreeNode(TreeNode root, HashSet<int> set)
+    {
+        foreach(var item in set)
+        {
+            if(root == null)
+            {
+                root = new TreeNode();
+            }
+            root.val = item;
+            set.Remove(root.val);
+            root.right = CreateTreeNode(root.right, set);
+        }
+        return root;
+    }
+}
