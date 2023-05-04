c54dea1f70b5c48f589972888d1f377df3dca435
Nikita Bukov <n.bukov@appricot.com>
2023-05-04 23:56:48 +0300

    Complete L104

---
+++ TrueLeetCode/Leetcode/Trees/L104.cs
@@ -0,0 +1,28 @@
+﻿namespace TrueLeetCode.Leetcode.Trees;
+
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/
+public class L104
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+        int answer = 0;
+        return Inorder(root, answer);
+    }
+
+    private int Inorder(TreeNode root, int currentDepth)
+    {
+        if (root != null)
+        {
+            currentDepth++;
+            int left = Inorder(root.left, currentDepth);
+            int right = Inorder(root.right, currentDepth);
+
+            return Math.Max(left, right);
+        }
+        return currentDepth;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using TrueLeetCode.Leetcode.Trees;
+
+var tree = new TreeNode
+{
+    left = new TreeNode(9),
+    right = new TreeNode(20)
+    {
+        left = new TreeNode(15),
+        right = new TreeNode(7)
+    }
+};
+
+var l = new L104();
+var r = l.MaxDepth(tree);
+Console.WriteLine();
