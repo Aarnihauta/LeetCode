845ac746ba407beff5922bf2e903d9b7a405e74a
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-05-06 15:42:23 +0300

    Complete L2236 (#20)
    Co-authored-by: Nikita Bukov <n.bukov@appricot.com>

---
+++ TrueLeetCode/Leetcode/Trees/L2236.cs
@@ -0,0 +1,10 @@
+﻿namespace TrueLeetCode.Leetcode.Trees;
+
+//https://leetcode.com/problems/root-equals-sum-of-children/
+public class L2236
+{
+    public bool CheckTree(TreeNode root)
+    {
+        return root.val == root.left.val + root.right.val;
+    }
+}
