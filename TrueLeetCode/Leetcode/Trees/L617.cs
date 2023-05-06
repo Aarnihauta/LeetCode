9677305d611177fe5f9a996bc3b1d3eee91cabe3
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-05-06 17:00:44 +0300

    Complete L617 (#21)
    Co-authored-by: Nikita Bukov <n.bukov@appricot.com>

---
+++ TrueLeetCode/Leetcode/Trees/L617.cs
@@ -0,0 +1,24 @@
+﻿namespace TrueLeetCode.Leetcode.Trees;
+
+//https://leetcode.com/problems/merge-two-binary-trees/
+public class L617
+{
+    public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+    {
+        if(root1 == null)
+        {
+            return root2;
+        }
+        if(root2 == null)
+        {
+            return root1;
+        }
+
+        return new TreeNode
+        {
+            val = root1.val + root2.val,
+            left = MergeTrees(root1.left, root2.left),
+            right = MergeTrees(root1.right, root2.right)
+        };
+    }
+}
