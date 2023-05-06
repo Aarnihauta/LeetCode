0e327c62247a1d148473c470ebc482134fcc660c
Nikita Bukov <n.bukov@appricot.com>
2023-05-06 16:50:38 +0300

    Complete L617

--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,17 +1,47 @@
 ﻿using TrueLeetCode.Leetcode.Trees;
 
-var tree = new TreeNode
+
+var root1 = new TreeNode
 {
-    val = 2,
-    left = new TreeNode(1),
-    right = new TreeNode
+    val = 1,
+    left = new TreeNode
     {
         val = 3,
-        left = new TreeNode(0),
-        right = new TreeNode(1)
+        left = new TreeNode
+        {
+            val = 5
+        }
+    },
+    right = new TreeNode
+    {
+        val = 2
     }
 };
 
-var l = new L2331();
-var r = l.EvaluateTree(tree);
+var root2 = new TreeNode
+{
+    val = 2,
+    left = new TreeNode
+    {
+        val = 1,
+        right = new TreeNode
+        {
+            val = 4
+        }
+    },
+    right = new TreeNode
+    {
+        val = 3,
+        right = new TreeNode
+        {
+            val = 7
+        }
+    }
+};
+
+
+var l = new L617();
+var r = l.MergeTrees(root1, root2);
+
+
 Console.WriteLine();
