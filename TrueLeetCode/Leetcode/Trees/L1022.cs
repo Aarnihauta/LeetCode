644cc96dd314bd19d28fb61482c17b820fb63bec
Nikita Bukov <n.bukov@appricot.com>
2023-05-08 00:29:06 +0300

    Complete L1022

---
+++ TrueLeetCode/Leetcode/Trees/L1022.cs
@@ -0,0 +1,33 @@
+﻿namespace TrueLeetCode.Leetcode.Trees;
+
+//https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
+public class L1022
+{
+    private int _sum;
+    public int SumRootToLeaf(TreeNode root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+        Traverse(root, 0);
+        return _sum;
+    }
+
+    private void Traverse(TreeNode root, int value)
+    {
+        if (root == null)
+        {
+            return;
+        }
+        
+        value = (value << 1) | root.val;
+        if (root.left == null && root.right == null)
+        {
+            _sum += value;
+        }
+
+        Traverse(root.left, value);
+        Traverse(root.right, value);
+    }
+}
