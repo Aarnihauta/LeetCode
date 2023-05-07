795e1d2bf0d290b9913292359af97fa237931797
Nikita Bukov <n.bukov@appricot.com>
2023-05-07 23:52:42 +0300

    Complete L700

--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using TrueLeetCode.Leetcode.Trees;
+
+var root1 = new TreeNode
+{
+    val = 18,
+    left = new TreeNode(2),
+    right = new TreeNode
+    {
+        val = 22,
+        right = new TreeNode(63)
+        {
+            right = new TreeNode(84)
+        }
+    }
+};
+
+var q = new L700();
+var c = q.SearchBST(root1, 63);
+Console.WriteLine(c);
