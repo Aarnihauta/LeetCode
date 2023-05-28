b3f042dcf18ac31a02f40146d40e0f54f933d8fc
Nikita <shinigamichan@yandex.ru>
2023-05-28 23:49:32 +0300

    Complete L144

---
+++ TrueLeetCode/Leetcode/Trees/L144.cs
@@ -0,0 +1,24 @@
+﻿namespace TrueLeetCode.Leetcode.Trees;
+
+//https://leetcode.com/problems/binary-tree-preorder-traversal
+public class L144
+{
+    public IList<int> PreorderTraversal(TreeNode root)
+    {
+        var list = new List<int>();
+        Preorder(root, list);
+        return list;
+    }
+
+    private void Preorder(TreeNode root, List<int> list)
+    {
+        if (root == null)
+        {
+            return;
+        }
+
+        list.Add(root.val);
+        Preorder(root.left, list); 
+        Preorder(root.right, list);
+    }
+}
