e4fa12ba02d663d5a4dfe2906115bfe434cadebf
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-05-31 21:56:41 +0300

    Complete L653 (#32)

---
+++ TrueLeetCode/Leetcode/Trees/L653.cs
@@ -0,0 +1,31 @@
+﻿namespace TrueLeetCode.Leetcode.Trees;
+public class L653
+{
+    private HashSet<int> _data = new HashSet<int>();
+    private bool _result = false;
+    public bool FindTarget(TreeNode root, int k)
+    {
+        Traverse(root, k);
+        return _result;
+    }
+
+    private void Traverse(TreeNode root, int k)
+    {
+        if(root == null)
+        {
+            return;
+        }
+
+        int complement = root.val;
+
+        if(_data.Contains(complement))
+        {
+            _result = true;
+            return;
+        }
+
+        _data.Add(root.val);
+        Traverse(root.left, k);
+        Traverse(root.right, k);
+    }
+}
