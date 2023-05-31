f5aa4a4ba4ac925c8e5afc602f461495a89779c1
Nikita <shinigamichan@yandex.ru>
2023-05-31 21:56:22 +0300

    Complete L653

--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,7 +1,1 @@
-﻿using TrueLeetCode.Leetcode.Strings;
-
-var l = new L609();
-
-string[] arr = { "root/a 1.txt(abcd) 2.txt(efsfgh) 3.txt(efsfgh)", "root/c 3.txt(abdfcd)", "root/c/d 4.txt(efggdfh)" };
-
-var res = l.FindDuplicate(arr);
+﻿
