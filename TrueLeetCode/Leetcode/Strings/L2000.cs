5efcc0a4815257e4a2763b7cb5ef3133f799188f
Nikita <shinigamichan@yandex.ru>
2023-06-04 22:26:51 +0300

    Complete L2000

---
+++ TrueLeetCode/Leetcode/Strings/L2000.cs
@@ -0,0 +1,18 @@
+﻿namespace TrueLeetCode.Leetcode.Strings;
+
+//https://leetcode.com/problems/reverse-prefix-of-word/
+public class L2000
+{
+    public string ReversePrefix(string word, char ch)
+    {
+        int index = word.IndexOf(ch);
+
+        if(index < 0)
+        {
+            return word;
+        }
+
+        string substr = word.Substring(0, index + 1);
+        return new string(substr.Reverse().ToArray()) + word[(index + 1)..];
+    }
+}
