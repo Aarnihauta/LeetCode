6a3fb6b5405ce3c3e834ab3cdec6ffaada088f24
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-06-04 10:10:07 +0300

    Complete L1108 (#33)

---
+++ TrueLeetCode/Leetcode/Strings/L1108.cs
@@ -0,0 +1,10 @@
+﻿namespace TrueLeetCode.Leetcode.Strings;
+
+//https://leetcode.com/problems/defanging-an-ip-address/
+public class L1108
+{
+    public string DefangIPaddr(string address)
+    {
+        return address.Replace(".", "[.]");
+    }
+}
