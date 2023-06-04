57c54f0582215f1b024a4fc0ab3634c9255ee0c1
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-06-04 10:27:53 +0300

    Complete L2011 (#34)
    * Complete L2011
    
    * WIP

---
+++ TrueLeetCode/Leetcode/Strings/L2011.cs
@@ -0,0 +1,24 @@
+﻿namespace TrueLeetCode.Leetcode.Strings;
+
+//https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+public class L2011
+{
+    public int FinalValueAfterOperations(string[] operations)
+    {
+        int result = 0;
+
+        foreach (var op in operations)
+        {
+            if (op.Contains("++"))
+            {
+                result++;
+            }
+            else if (op.Contains("--"))
+            {
+                result--;
+            }
+        }
+
+        return result;
+    }
+}
