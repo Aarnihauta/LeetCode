346b21b06e11afe0c4711a5cedcea7f4c5048f25
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-06-04 10:38:14 +0300

    Complete L1678 (#36)

---
+++ TrueLeetCode/Leetcode/Strings/L1678.cs
@@ -0,0 +1,12 @@
+﻿namespace TrueLeetCode.Leetcode.Strings;
+
+//https://leetcode.com/problems/goal-parser-interpretation/
+public class L1678
+{
+    public string Interpret(string command)
+    {
+        return command
+            .Replace("()", "o")
+            .Replace("(al)", "al");
+    }
+}
