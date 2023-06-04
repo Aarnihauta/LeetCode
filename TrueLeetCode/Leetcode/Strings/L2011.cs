a159a07abad66bfd0177a70ada4399ad026b1517
Nikita <shinigamichan@yandex.ru>
2023-06-04 10:26:39 +0300

    Complete L2011

---
+++ TrueLeetCode/Leetcode/Strings/L2011.cs
@@ -0,0 +1,22 @@
+﻿namespace TrueLeetCode.Leetcode.Strings;
+public class L2011
+{
+    public int FinalValueAfterOperations(string[] operations)
+    {
+        int result = 0;
+
+        foreach (var op in operations)
+        {
+            if (op.Contains("++"))
+            {
+                result++;
+            }
+            else if (op.Contains("--"))
+            {
+                result--;
+            }
+        }
+
+        return result;
+    }
+}
