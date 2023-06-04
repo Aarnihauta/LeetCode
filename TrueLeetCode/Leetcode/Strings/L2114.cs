d61a9a2a8ef51b8e4da2f126a4cd3777ed7ecdbd
Nikita <shinigamichan@yandex.ru>
2023-06-04 10:32:28 +0300

    Complete L2114

---
+++ TrueLeetCode/Leetcode/Strings/L2114.cs
@@ -0,0 +1,16 @@
+﻿namespace TrueLeetCode.Leetcode.Strings;
+
+//https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+public class L2114
+{
+    public int MostWordsFound(string[] sentences)
+    {
+        int max = 0;
+        foreach(var sentence in sentences)
+        {
+            max = Math.Max(max, sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length);
+        }
+
+        return max;
+    }
+}
