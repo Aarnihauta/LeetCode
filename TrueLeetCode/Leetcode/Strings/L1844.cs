8193ce9b0461bd71bcf263d4161dfe9fb5cedde4
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-06-04 10:59:12 +0300

    Complete L1844 (#37)

---
+++ TrueLeetCode/Leetcode/Strings/L1844.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace TrueLeetCode.Leetcode.Strings;
+
+//https://leetcode.com/problems/replace-all-digits-with-characters/
+public class L1844
+{
+    public string ReplaceDigits(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sb.Append(s[i]);
+            }
+            else
+            {
+                int shift = s[i] - '0';
+                sb.Append(Shift(s[i - 1], shift));
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    private char Shift(char c, int shift)
+    {
+        return (char)(c + shift);
+    }
+}
