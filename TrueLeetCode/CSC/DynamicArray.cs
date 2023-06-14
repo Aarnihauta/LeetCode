47591b93345add43f9fe15a26f012cce0e88016a
Nikita <shinigamichan@yandex.ru>
2023-06-14 22:21:02 +0300

    WIP: динамический массив с мультипликативной схемой перевыделения

---
+++ TrueLeetCode/CSC/DynamicArray.cs
@@ -0,0 +1,41 @@
+﻿namespace TrueLeetCode.CSC;
+
+/// <summary>
+/// Динамический массив с мультипликативной схемой перевыделения
+/// Суммарно имеет n операций добавления, соответственно сложность добавления 
+/// </summary>
+public class DynamicMultipicativeArray<T>
+{
+    private T[] _array;
+    private int _size;
+
+    public void Add(T data)
+    {
+        if (_size == _array.Length)
+        {
+            Grow(2);
+        }
+        _array[_size] = data;
+        _size++;
+    }
+
+    public bool Remove(T item)
+    {
+        int index = Array.IndexOf(_array, item);
+
+        if(index > 0)
+        {
+            _array[index] = default;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Grow(int multiplier)
+    {
+        var newArr = new T[_size * multiplier];
+        Array.Copy(_array, newArr, _size);
+        _array = newArr;
+    }
+}
