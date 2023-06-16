a2338dfa029dcd0b56d630309a14e8ec5d198116
Nikita <shinigamichan@yandex.ru>
2023-06-16 22:57:03 +0300

    Добавлен класс DynamicArray с мультипликативной и аддитивной имплементацией

--- TrueLeetCode/CSC/DynamicArray.cs
+++ TrueLeetCode/CSC/DynamicArray.cs
@@ -1,9 +1,6 @@
 ﻿namespace TrueLeetCode.CSC;
 
-/// <summary>
-/// Динамический массив с мультипликативной схемой перевыделения
-/// Суммарно имеет n операций добавления, соответственно сложность добавления 
-/// </summary>
+
 public class DynamicMultipicativeArray<T>
 {
     private T[] _array;
@@ -35,6 +32,43 @@ public class DynamicMultipicativeArray<T>
     private void Grow(int multiplier)
     {
         var newArr = new T[_size * multiplier];
+        Array.Copy(_array, newArr, _size);
+        _array = newArr;
+    }
+}
+
+
+public class DynamicAdditiveArray<T>
+{
+    private T[] _array;
+    private int _size;
+
+    public void Add(T data)
+    {
+        if (_size == _array.Length)
+        {
+            Grow(2);
+        }
+        _array[_size] = data;
+        _size++;
+    }
+
+    public bool Remove(T item)
+    {
+        int index = Array.IndexOf(_array, item);
+
+        if (index > 0)
+        {
+            _array[index] = default;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Grow(int multiplier)
+    {
+        var newArr = new T[_size + 10];
         Array.Copy(_array, newArr, _size);
         _array = newArr;
     }
