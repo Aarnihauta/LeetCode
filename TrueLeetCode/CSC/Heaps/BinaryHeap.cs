534638b742af61f2892c6fde4b8b6e32a3937f28
Nikita <shinigamichan@yandex.ru>
2023-06-18 23:02:41 +0300

    WIP binary heap

---
+++ TrueLeetCode/CSC/Heaps/BinaryHeap.cs
@@ -0,0 +1,29 @@
+﻿namespace TrueLeetCode.CSC.Heap;
+public class BinaryHeap
+{
+    private List<int> _items;
+    public int Count => _items.Count;
+
+    public int GetMax()
+    {
+        if(Count > 0)
+        {
+            return _items[0];
+        }
+        return default(int);
+    }
+
+    public void Add(int item)
+    {
+        _items.Add(item);
+        var currentIndex = Count - 1;
+        var parentIndex = (currentIndex - 1) / 2;
+
+        while(currentIndex > 0 && _items[parentIndex] < _items[currentIndex])
+        {
+            var temp = _items[currentIndex];
+            _items[currentIndex] = _items[parentIndex];
+            _items[parentIndex] = temp;
+        }
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿int a = int.MaxValue;
+int b = int.MaxValue - 1;
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
