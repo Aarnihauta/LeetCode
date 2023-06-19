1853e9651723937fde21252ca733259eb18b98fc
Nikita <shinigamichan@yandex.ru>
2023-06-19 23:26:47 +0300

    WIP: Binary heap

--- TrueLeetCode/CSC/Heaps/BinaryHeap.cs
+++ TrueLeetCode/CSC/Heaps/BinaryHeap.cs
@@ -4,7 +4,7 @@ public class BinaryHeap
     private List<int> _items;
     public int Count => _items.Count;
 
-    public int GetMax()
+    public int Peek()
     {
         if(Count > 0)
         {
@@ -13,17 +13,30 @@ public class BinaryHeap
         return default(int);
     }
 
-    public void Add(int item)
+    public void Enqueue(int item)
     {
         _items.Add(item);
         var currentIndex = Count - 1;
-        var parentIndex = (currentIndex - 1) / 2;
+        var parentIndex = GetParentIndex(currentIndex);
 
-        while(currentIndex > 0 && _items[parentIndex] < _items[currentIndex])
+        while (currentIndex > 0 && _items[parentIndex] < _items[currentIndex])
         {
-            var temp = _items[currentIndex];
-            _items[currentIndex] = _items[parentIndex];
-            _items[parentIndex] = temp;
+            Swap(currentIndex, parentIndex);
+
+            currentIndex = parentIndex;
+            parentIndex = GetParentIndex(currentIndex);
         }
+    }
+
+    private int GetParentIndex(int currentIndex)
+    {
+        return (currentIndex - 1) / 2;
+    }
+
+    private void Swap(int currentIndex, int parentIndex)
+    {
+        var temp = _items[currentIndex];
+        _items[currentIndex] = _items[parentIndex];
+        _items[parentIndex] = temp;
     }
 }
