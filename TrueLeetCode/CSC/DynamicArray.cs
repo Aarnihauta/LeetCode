bb692bd3779e4f096b808e80e45a72e2408f41f5
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-06-20 22:29:29 +0300

    Feature/csc (#40)
    * WIP: динамический массив с мультипликативной схемой перевыделения
    
    * Добавлен класс DynamicArray с мультипликативной и аддитивной имплементацией
    
    * WIP binary heap
    
    * WIP: Binary heap
    
    * Добавлен метод сортировки
    
    * Добавлен метод SiftUp в BinaryHeap
    
    * Добавлена проверка на индексы в siftUp
    
    * Исправлен баг с сортировкой при инициализации кучи на основе коллекции

---
+++ TrueLeetCode/CSC/DynamicArray.cs
@@ -0,0 +1,75 @@
+﻿namespace TrueLeetCode.CSC;
+
+
+public class DynamicMultipicativeArray<T>
+{
+    private T[] _array;
+    private int _size;
+
+    public void Add(T data)
+    {
+        if (_size == _array.Length)
+        {
+            Grow(2);
+        }
+        _array[_size] = data;
+        _size++;
+    }
+
+    public bool Remove(T item)
+    {
+        int index = Array.IndexOf(_array, item);
+
+        if(index > 0)
+        {
+            _array[index] = default;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Grow(int multiplier)
+    {
+        var newArr = new T[_size * multiplier];
+        Array.Copy(_array, newArr, _size);
+        _array = newArr;
+    }
+}
+
+
+public class DynamicAdditiveArray<T>
+{
+    private T[] _array;
+    private int _size;
+
+    public void Add(T data)
+    {
+        if (_size == _array.Length)
+        {
+            Grow(2);
+        }
+        _array[_size] = data;
+        _size++;
+    }
+
+    public bool Remove(T item)
+    {
+        int index = Array.IndexOf(_array, item);
+
+        if (index > 0)
+        {
+            _array[index] = default;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Grow(int multiplier)
+    {
+        var newArr = new T[_size + 10];
+        Array.Copy(_array, newArr, _size);
+        _array = newArr;
+    }
+}
---
+++ TrueLeetCode/CSC/Heaps/BinaryHeap.cs
@@ -0,0 +1,99 @@
+﻿namespace TrueLeetCode.CSC.Heap;
+public class BinaryHeap
+{
+    private List<int> _items = new List<int>();
+
+    public BinaryHeap()
+    {
+
+    }
+    public BinaryHeap(List<int> items)
+    {
+        _items = items;
+        for (int i = Count; i >= 0; i--)
+        {
+            SiftUp(i);
+        }
+    }
+
+    public int Count => _items.Count;
+
+    public int Peek()
+    {
+        if (Count > 0)
+        {
+            return _items[0];
+        }
+
+        return default(int);
+    }
+
+    public int GetMax()
+    {
+        var result = _items[0];
+        _items[0] = _items[^1];
+        _items.RemoveAt(Count - 1);
+
+        SiftUp(0);
+
+        return result;
+    }
+
+    public void Add(int item)
+    {
+        _items.Add(item);
+        var currentIndex = Count - 1;
+        var parentIndex = GetParentIndex(currentIndex);
+
+        while (currentIndex > 0 && _items[parentIndex] < _items[currentIndex])
+        {
+            Swap(currentIndex, parentIndex);
+
+            currentIndex = parentIndex;
+            parentIndex = GetParentIndex(currentIndex);
+        }
+    }
+
+    private int GetParentIndex(int currentIndex)
+    {
+        return (currentIndex - 1) / 2;
+    }
+
+    private void Swap(int currentIndex, int parentIndex)
+    {
+        var temp = _items[currentIndex];
+        _items[currentIndex] = _items[parentIndex];
+        _items[parentIndex] = temp;
+    }
+
+    private void SiftUp(int currentIndex)
+    {
+        int leftIndex;
+        int rightIndex;
+        int maxIndex = currentIndex;
+
+        while (currentIndex < Count)
+        {
+            leftIndex = 2 * currentIndex + 1;
+            rightIndex = 2 * currentIndex + 2;
+
+            if (leftIndex < Count && _items[leftIndex] > _items[maxIndex])
+            {
+                maxIndex = leftIndex;
+            }
+
+            if (rightIndex < Count && _items[rightIndex] > _items[maxIndex])
+            {
+                maxIndex = rightIndex;
+            }
+
+            if (maxIndex == currentIndex)
+            {
+                break;
+            }
+
+            Swap(currentIndex, maxIndex);
+            currentIndex = maxIndex;
+        }
+    }
+}
