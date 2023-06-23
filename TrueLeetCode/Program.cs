63cc1308c4dbc08af1f0723143028cc8d76b07d7
Nikita <shinigamichan@yandex.ru>
2023-06-23 23:15:03 +0300

    Complete L1971

--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,18 +1,2 @@
 ﻿using TrueLeetCode.CSC.Heap;
 
-var heap = new BinaryHeap();
-
-heap.Add(20);
-heap.Add(11);
-heap.Add(17);
-heap.Add(7);
-heap.Add(4);
-heap.Add(13);
-heap.Add(15);
-heap.Add(14);
-
-
-while(heap.Count > 0)
-{
-    Console.WriteLine(heap.GetMax()); 
-}
