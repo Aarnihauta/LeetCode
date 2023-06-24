f7b5c4d17b85d74765a471f04eaa131e38a43452
Nikita <shinigamichan@yandex.ru>
2023-06-24 12:14:55 +0300

    Complete L55

---
+++ TrueLeetCode/Leetcode/DP/L55.cs
@@ -0,0 +1,23 @@
+﻿namespace TrueLeetCode.Leetcode.DP;
+
+//https://leetcode.com/problems/jump-game/description/
+public class L55
+{
+    //Конец массива равен true, поскольку это означает, что мы подошли к решению.
+    //Работая в обратном направлении по массиву, мы хотим проверить, может ли наше текущее число привести нас к ближайшему true
+    //Если ближайшим значением true является start, то все решение доступно для перехода.
+    public bool CanJump(int[] nums)
+    {
+        int res = nums.Length - 1;
+
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            if (i + nums[i] >= res)
+            {
+                res = i;
+            }
+        }
+
+        return res == 0;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using TrueLeetCode.Leetcode.DP;
+
+int[] arr = { 2, 3, 0, 1, 4 };
+var l = new L55();
+
+l.CanJump(arr);
