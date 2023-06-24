2e2a3f142e0fa6d1d7b51ab2259ea7b573fb8d10
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-06-24 11:33:07 +0300

    Complete L1025 (#43)

---
+++ TrueLeetCode/Leetcode/DP/L1025.cs
@@ -0,0 +1,10 @@
+﻿namespace TrueLeetCode.Leetcode.DP;
+
+//https://leetcode.com/problems/divisor-game/description/
+public class L1025
+{
+    public bool DivisorGame(int n)
+    {
+        return n % 2 == 0;
+    }
+}
