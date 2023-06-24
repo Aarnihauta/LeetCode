d5aed602ba5e356cd72a22b47e23a829f4ceea83
Nikita <shinigamichan@yandex.ru>
2023-06-24 10:36:28 +0300

    Complete L509

---
+++ TrueLeetCode/Leetcode/DP/L509.cs
@@ -0,0 +1,23 @@
+﻿namespace TrueLeetCode.Leetcode.DP;
+public class L509
+{
+    public int Fib(int n)
+    {
+        if(n <= 1)
+        {
+            return 1;
+        }
+
+        int n1 = 0;
+        int n2 = 1;
+        int next = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            next = n1 + n2;
+            n1 = n2;
+            n2 = next;
+        }
+
+        return next;
+    }
+}
