10831ba67d2d4ec69137f8689b0a8a778654aa70
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-07-01 00:14:01 +0300

    Complete L746 (#48)

---
+++ TrueLeetCode/Leetcode/DP/L746.cs
@@ -0,0 +1,17 @@
+﻿namespace TrueLeetCode.Leetcode.DP;
+
+//https://leetcode.com/problems/min-cost-climbing-stairs/
+public class L746
+{
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        int l = cost.Length;
+
+        for (int i = 2; i < cost.Length; i++)
+        {
+            cost[i] += Math.Min(cost[i - 1], cost[i - 2]);
+        }
+
+        return Math.Min(cost[l - 1], cost[l - 2]);
+    }
+}
