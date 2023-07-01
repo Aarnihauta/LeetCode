e1dbc725396c5ba6bdb8a2471d7df7170ca3c41c
Nikita <62142248+Aarnihauta@users.noreply.github.com>
2023-07-01 11:16:36 +0300

    Complete L118 (#49)

---
+++ TrueLeetCode/Leetcode/DP/L118.cs
@@ -0,0 +1,25 @@
+﻿namespace TrueLeetCode.Leetcode.DP;
+public class L118
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        var dp = new int[numRows][];
+
+        dp[0] = new int[] { 1 };
+         
+        for (int i = 1; i < numRows; i++)
+        {
+            int k = dp[i - 1].Length;
+            dp[i] = new int[k + 1];
+            dp[i][0] = 1;
+            dp[i][k] = 1;
+
+            for (int j = 1; j < k; j++)
+            {
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+            }
+        }
+
+        return dp;
+    }
+}
