9d90eb8340f7fbefccab07dd8e66ddaddafa8041
Nikita <shinigamichan@yandex.ru>
2023-07-04 10:33:51 +0300

    Complete L1689

---
+++ TrueLeetCode/Leetcode/Greedy/L1689.cs
@@ -0,0 +1,10 @@
+﻿namespace TrueLeetCode.Leetcode.Greedy;
+
+//https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+public class L1689
+{
+    public int MinPartitions(string s)
+    {
+        return s.Max() - '0';
+    }
+}
