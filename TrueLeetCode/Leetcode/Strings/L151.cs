bf77e71e15c7a431dc49016fa3cf46d2f228a03f
Nikita <shinigamichan@yandex.ru>
2023-07-04 21:48:27 +0300

    Complete L151

---
+++ TrueLeetCode/Leetcode/Strings/L151.cs
@@ -0,0 +1,11 @@
+﻿namespace TrueLeetCode.Leetcode.Strings;
+public class L151
+{
+    public string ReverseWords(string s)
+    {
+        var arr = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        Array.Reverse(arr);
+
+        return string.Join(' ', arr);
+    }
+}
