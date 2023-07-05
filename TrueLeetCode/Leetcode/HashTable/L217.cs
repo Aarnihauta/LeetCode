5ca2a6a511c2852b52e281091f8688f1a5719106
Nikita <shinigamichan@yandex.ru>
2023-07-05 15:47:15 +0300

    Complete L217

---
+++ TrueLeetCode/Leetcode/HashTable/L217.cs
@@ -0,0 +1,8 @@
+﻿namespace TrueLeetCode.Leetcode.HashTable;
+public class L217
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        return new HashSet<int>(nums).Count != nums.Length;
+    }
+}
