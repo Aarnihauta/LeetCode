4f960931df78558c07e9c34aa162b44bf09436d6
Nikita <shinigamichan@yandex.ru>
2023-07-05 16:45:14 +0300

    Complete L242

---
+++ TrueLeetCode/Leetcode/HashTable/L242.cs
@@ -0,0 +1,10 @@
+﻿namespace TrueLeetCode.Leetcode.HashTable;
+
+//https://leetcode.com/problems/valid-anagram
+public class L242
+{
+    public bool IsAnagram(string s, string t)
+    {
+        return s.OrderBy(x => x).SequenceEqual(t.OrderBy(x => x));
+    }
+}
