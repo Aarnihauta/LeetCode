945b90d602298feda5837f83d9da8850f4db381c
Nikita <shinigamichan@yandex.ru>
2023-07-07 18:52:49 +0300

    Complete L36

---
+++ TrueLeetCode/Leetcode/HashTable/L36.cs
@@ -0,0 +1,41 @@
+﻿namespace TrueLeetCode.Leetcode.HashTable;
+
+//https://leetcode.com/problems/valid-sudoku/
+public class L36
+{
+    public bool IsValidSudoku(char[][] board)
+    {
+        HashSet<char>[] rows = new HashSet<char>[9];
+        HashSet<char>[] cols = new HashSet<char>[9];
+        HashSet<char>[] squares = new HashSet<char>[9];
+        for (int i = 0; i < 9; i++)
+        {
+            rows[i] = new HashSet<char>();
+            cols[i] = new HashSet<char>();
+            squares[i] = new HashSet<char>();
+        }
+
+        for (int r = 0; r < board.Length; r++)
+        {
+            for (int c = 0; c < board[r].Length; c++)
+            {
+                char current = board[r][c];
+                int boxIndex = (3 * (r / 3)) + (c / 3);
+
+                if (current == '.')
+                {
+                    continue;
+                }
+
+                if (!rows[r].Add(current) ||
+                    !cols[c].Add(current) ||
+                    !squares[boxIndex].Add(current))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,5 +1,37 @@
-using TrueLeetCode.Leetcode.Common;
+using TrueLeetCode.Leetcode.HashTable;
 
-var l = new L238();
+var l = new L36();
 
-l.ProductExceptSelf(new int[] { 1, 2, 3, 4 });
+char[][] table = new char[][]
+{
+
+     new char[]{'5','3','.','.','7','.','.','.','.'}
+    ,new char[]{'6','.','.','1','9','5','.','.','.'}
+    ,new char[]{'.','9','8','.','.','.','.','6','.'}
+    ,new char[]{'8','.','.','.','6','.','.','.','3'}
+    ,new char[]{'4','.','.','8','.','3','.','.','1'}
+    ,new char[]{'7','.','.','.','2','.','.','.','6'}
+    ,new char[]{'.','6','.','.','.','.','2','8','.'}
+    ,new char[]{'.','.','.','4','1','9','.','.','5'}
+    ,new char[]{'.','.','.','.','8','.','.','7','9'}
+};
+
+for (int i = 3; i < 6; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write(table[i][j]);
+    }
+    Console.WriteLine();
+}
+
+//for (int i = 0; i < 9; i++)
+//{
+//    for (int j = 0; j < 9; j++)
+//    {
+//        Console.Write(table[i][j] + " ");
+//    }
+//    Console.WriteLine();
+//}
+
+l.IsValidSudoku(table);
