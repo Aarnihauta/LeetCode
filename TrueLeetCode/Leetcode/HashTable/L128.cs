1fd6475379ef0790d5edc8d62cd077ab5b44aa45
Nikita <shinigamichan@yandex.ru>
2023-07-07 21:51:57 +0300

    Complete L128

---
+++ TrueLeetCode/Leetcode/HashTable/L128.cs
@@ -0,0 +1,43 @@
+﻿namespace TrueLeetCode.Leetcode.HashTable;
+
+//https://leetcode.com/problems/longest-consecutive-sequence/
+public class L128
+{
+    public int LongestConsecutive(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+
+        int longestChain = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(nums[i]))
+            {
+                nums[i]++;
+            }
+            else
+            {
+                dict.Add(nums[i], 1);
+            }
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var key = nums[i] - 1;
+
+            if (!dict.ContainsKey(key))
+            {
+                var next = nums[i];
+                int current = 0;
+                while (dict.ContainsKey(next))
+                {
+                    current += dict[next];
+                    longestChain = Math.Max(longestChain, current);
+                    next++;
+                }
+            }
+        }
+
+        return longestChain;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,37 +1,5 @@
 using TrueLeetCode.Leetcode.HashTable;
 
-var l = new L36();
+L128 l = new L128();
 
-char[][] table = new char[][]
-{
-
-     new char[]{'5','3','.','.','7','.','.','.','.'}
-    ,new char[]{'6','.','.','1','9','5','.','.','.'}
-    ,new char[]{'.','9','8','.','.','.','.','6','.'}
-    ,new char[]{'8','.','.','.','6','.','.','.','3'}
-    ,new char[]{'4','.','.','8','.','3','.','.','1'}
-    ,new char[]{'7','.','.','.','2','.','.','.','6'}
-    ,new char[]{'.','6','.','.','.','.','2','8','.'}
-    ,new char[]{'.','.','.','4','1','9','.','.','5'}
-    ,new char[]{'.','.','.','.','8','.','.','7','9'}
-};
-
-for (int i = 3; i < 6; i++)
-{
-    for (int j = 0; j < 3; j++)
-    {
-        Console.Write(table[i][j]);
-    }
-    Console.WriteLine();
-}
-
-//for (int i = 0; i < 9; i++)
-//{
-//    for (int j = 0; j < 9; j++)
-//    {
-//        Console.Write(table[i][j] + " ");
-//    }
-//    Console.WriteLine();
-//}
-
-l.IsValidSudoku(table);
+l.LongestConsecutive(new int[] { 100, 4, 200, 1, 3, 2 });
