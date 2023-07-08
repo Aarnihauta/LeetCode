84916fef671639d0b1724b93bf6a2445d3abe399
Nikita <shinigamichan@yandex.ru>
2023-07-08 18:05:20 +0300

    Complete L150

---
+++ TrueLeetCode/Leetcode/Stack/L150.cs
@@ -0,0 +1,40 @@
+﻿namespace TrueLeetCode.Leetcode.Stack;
+
+//https://leetcode.com/problems/evaluate-reverse-polish-notation/
+public class L150
+{
+    public int EvalRPN(string[] tokens)
+    {
+        var stack = new Stack<int>();
+
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            var c = tokens[i];
+
+            if (c == "*" || c == "/" || c == "+" || c == "-")
+            {
+                var right = Convert.ToInt32(stack.Pop());
+                var left = Convert.ToInt32(stack.Pop());
+
+                var val = Evaluate(left, right, c);
+                stack.Push(val);
+            }
+            else
+            {
+                stack.Push(Convert.ToInt32(c));
+            }
+        }
+        return stack.Peek();
+    }
+
+    private int Evaluate(int left, int right, string op)
+    {
+        return op switch
+        {
+            "*" => left * right,
+            "/" => left / right,
+            "-" => left - right,
+            "+" => left + right,
+        };
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,18 +1,5 @@
-
 using TrueLeetCode.Leetcode.Stack;
 
-MinStack stack = new MinStack();
+var l = new L150();
 
-stack.Push(-10);
-stack.Push(14);
-stack.Push(-20);
-var p = stack.Top();
-
-stack.Pop();
-stack.Push(10);
-stack.Push(-7);
-stack.Push(-7);
-stack.Pop();
-stack.Pop();
-
-Console.WriteLine();
+l.EvalRPN(new string[] { "4", "13", "5", "/", "+" });
