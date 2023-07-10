41d1ce974f15e8203ab766361e354f69e70ba26b
Nikita <shinigamichan@yandex.ru>
2023-07-11 00:20:47 +0300

    Complete L167

--- TrueLeetCode/Leetcode/Pointers/L167.cs
+++ TrueLeetCode/Leetcode/Pointers/L167.cs
@@ -5,60 +5,27 @@ public class L167
 {
     public int[] TwoSum(int[] numbers, int target)
     {
-        for (int i = 0; i < numbers.Length; i++)
+        int l = 0;
+        int r = numbers.Length - 1;
+        while (l <= r)
         {
-            int val = target - numbers[i];
-            int searchIndex = BinarySearch(numbers, val, i);
+            int left = numbers[l];
+            int right = numbers[r];
 
-            if (searchIndex > 0)
+            if (left + right > target)
             {
-                return new int[] { i + 1, searchIndex + 1 };
+                r--;
             }
-        }
-        return new int[2];
-    }
-
-    private int BinarySearch(int[] numbers, int target, int skipIndex)
-    {
-        int start = 0;
-        int end = numbers.Length - 1;
-        while (start <= end)
-        {
-            int mid = (start + end) / 2;
-            if(target == numbers[mid])
+            else if (left + right < target)
             {
-                if(mid == skipIndex)
-                {
-                    if (mid - 1 >= 0 && numbers[mid - 1] == target)
-                    {
-                        return mid - 1;
-                    }
-                    else if (mid + 1 < numbers.Length && numbers[mid + 1] == target)
-                    {
-                        return mid + 1;
-                    }
-                    else
-                    {
-                        return -1;
-                    }
-                }
-                else
-                {
-                    return mid;
-                }
+                l++;
             }
             else
             {
-                if(target < numbers[mid])
-                {
-                    end = mid - 1;
-                }
-                else
-                {
-                    start = mid + 1;
-                }
+                return new int[] { l + 1, r + 1 };
             }
         }
-        return -1;
+
+        return null;
     }
 }
