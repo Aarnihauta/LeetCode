4b58dafc2fd14b4bf2b57c61d03a57f68b08b828
Nikita <shinigamichan@yandex.ru>
2023-07-11 00:04:45 +0300

    Complete L167

---
+++ TrueLeetCode/Leetcode/Pointers/L167.cs
@@ -0,0 +1,64 @@
+﻿namespace TrueLeetCode.Leetcode.Pointers;
+
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+public class L167
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int val = target - numbers[i];
+            int searchIndex = BinarySearch(numbers, val, i);
+
+            if (searchIndex > 0)
+            {
+                return new int[] { i + 1, searchIndex + 1 };
+            }
+        }
+        return new int[2];
+    }
+
+    private int BinarySearch(int[] numbers, int target, int skipIndex)
+    {
+        int start = 0;
+        int end = numbers.Length - 1;
+        while (start <= end)
+        {
+            int mid = (start + end) / 2;
+            if(target == numbers[mid])
+            {
+                if(mid == skipIndex)
+                {
+                    if (mid - 1 >= 0 && numbers[mid - 1] == target)
+                    {
+                        return mid - 1;
+                    }
+                    else if (mid + 1 < numbers.Length && numbers[mid + 1] == target)
+                    {
+                        return mid + 1;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+            else
+            {
+                if(target < numbers[mid])
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
