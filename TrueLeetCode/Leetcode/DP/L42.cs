15f953f8d115b38aac1d6821172d61ba6bfcd915
Nikita <shinigamichan@yandex.ru>
2023-07-11 17:57:15 +0300

    Complete L704

---
+++ TrueLeetCode/Leetcode/DP/L42.cs
@@ -0,0 +1,11 @@
+﻿namespace TrueLeetCode.Leetcode.DP;
+
+//https://leetcode.com/problems/trapping-rain-water/
+public class L42
+{
+    public int Trap(int[] height)
+    {
+        //TODO: L42
+        return 0;
+    }
+}
--- TrueLeetCode/Leetcode/HashTable/L84.cs
+++ TrueLeetCode/Leetcode/HashTable/L84.cs
@@ -5,29 +5,6 @@ public class L84
 {
     public int LargestRectangleArea(int[] heights)
     {
-        //var stack = new Stack<(int height, int index)>();
-        //int max = 0;
-        //for (int i = 0; i < heights.Length; i++)
-        //{
-        //    var current = i;
-        //    while (stack.Any() && stack.Peek().height > heights[i])
-        //    {
-        //        var rectangle = stack.Pop();
-        //        current = rectangle.index;
-        //        max = Math.Max(max, (i - current) * rectangle.height);
-        //    }
-
-        //    stack.Push((heights[i], current));
-        //}
-
-        //while (stack.Any())
-        //{
-        //    var t = stack.Pop();
-        //    max = Math.Max(max, t.height * (heights.Length - t.index));
-        //}
-
-        //return max;
-
         //TODO: L84
         return 0;
     }
