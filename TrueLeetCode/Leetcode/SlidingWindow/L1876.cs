de92213b771cac5fc022149c0062360711f88c51
Nikita <shinigamichan@yandex.ru>
2023-07-12 18:09:59 +0300

    Complete L1876

---
+++ TrueLeetCode/Leetcode/SlidingWindow/L1876.cs
@@ -0,0 +1,22 @@
+﻿namespace TrueLeetCode.Leetcode.SlidingWindow;
+
+//https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/
+public class L1876
+{
+    public int CountGoodSubstrings(string s)
+    {
+        int res = 0;
+        int i = 3;
+        while (i <= s.Length)
+        {
+            var dist = s[(i - 3)..i].Distinct().ToList();
+            if (dist.Count() == 3)
+            {
+                res++;
+            }
+            i++;
+        }
+
+        return res;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,12 +1,5 @@
-using TrueLeetCode.Leetcode.BinarySearch;
+using TrueLeetCode.Leetcode.SlidingWindow;
 
-var l = new L74();
+var l = new L1876();
 
-var arr = new int[][]
-{
-    new int[] {1, 3, 5, 7 },
-    new int[] {10, 11, 16, 20 },
-    new int[] {23, 30, 34, 60 }
-};
-
-l.SearchMatrix(arr, 3);
+l.CountGoodSubstrings("aababcabc");
