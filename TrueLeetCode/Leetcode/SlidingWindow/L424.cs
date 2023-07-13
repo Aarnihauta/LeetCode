a1cc6e288046d1b8a2fd2b543a00cdf6408eb1fb
Nikita <shinigamichan@yandex.ru>
2023-07-13 21:41:01 +0300

    Complete L424

--- TrueLeetCode/Leetcode/SlidingWindow/L424.cs
+++ TrueLeetCode/Leetcode/SlidingWindow/L424.cs
@@ -5,7 +5,34 @@ public class L424
 {
     public int CharacterReplacement(string s, int k)
     {
-        //TODO: L424
-        return 0;
+        var dict = new Dictionary<char, int>();
+        int res = 0;
+        int l = 0;
+
+        for (int r = 0; r < s.Length; r++)
+        {
+            Add(dict, s, r);
+            while ((r - l + 1) - dict.Max(x => x.Value) > k)
+            {
+                dict[s[l]] -= 1;
+                l += 1;
+            }
+
+            res = Math.Max(res, r - l + 1);
+        }
+
+        return res;
+    }
+
+    private void Add(Dictionary<char, int> dict, string s, int p)
+    {
+        if (dict.ContainsKey(s[p]))
+        {
+            dict[s[p]]++;
+        }
+        else
+        {
+            dict.Add(s[p], 1);
+        }
     }
 }
