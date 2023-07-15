d50916d166afece0748b4ed849171249d16616c8
Nikita <shinigamichan@yandex.ru>
2023-07-15 22:37:27 +0300

    Complete L912

---
+++ TrueLeetCode/Leetcode/Arrays/L912.cs
@@ -0,0 +1,26 @@
+﻿namespace TrueLeetCode.Leetcode.Arrays;
+
+//https://leetcode.com/problems/sort-an-array/
+public class L912
+{
+    public int[] SortArray(int[] nums)
+    {
+        var queue = new PriorityQueue<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            queue.Enqueue(nums[i], nums[i]);
+        }
+
+        var result = new int[nums.Length];
+
+        int j = 0;
+        while (queue.Count > 0)
+        {
+            result[j] = queue.Dequeue();
+            j++;
+        }
+
+        return result;
+    }
+}
--- TrueLeetCode/Program.cs
+++
@@ -1,5 +0,0 @@
-using TrueLeetCode.Leetcode;
-
-var l = new L206();
-
-l.ReverseList(new ListNode(1, new ListNode(2, new ListNode(3))));
