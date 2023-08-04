d755ed2a9c7c4744d9aa4abccbf49be78bd19362
Nikita <shinigamichan@yandex.ru>
2023-08-04 20:40:52 +0300

    Complete L153

---
+++ TrueLeetCode/Leetcode/Common/L153.cs
@@ -0,0 +1,26 @@
+﻿namespace TrueLeetCode.Leetcode.Common;
+
+//https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+public class L153
+{
+    public int FindMin(int[] nums)
+    {
+        int l = 0;
+        int r = nums.Length - 1;
+
+        while (l < r)
+        {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > nums[r])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid;
+            }
+        }
+
+        return nums[l];
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,5 +1,7 @@
-using TrueLeetCode.Leetcode.Arrays;
+using TrueLeetCode.Leetcode.Common;
 
-var l = new L2089();
+var l = new L153();
 
-l.TargetIndices(new[] { 1, 0, 5, 0, 2 }, 5);
+var q = l.FindMin(new int[] { 3, 4, 5, 1, 2 });
+
+Console.WriteLine(q);
