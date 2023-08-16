72dc369faa06d9891c40b29a02c7a193ee3b045f
Nikita Bukov <n.bukov@smartway.today>
2023-08-16 21:25:54 +0300

    Добавлена реализация функции Эйлера

---
+++ TrueLeetCode/Emax/Euler.cs
@@ -0,0 +1,29 @@
+﻿namespace TrueLeetCode.Emax;
+public class Euler
+{
+    /// <summary>
+    /// phi(n)
+    /// </summary>
+    public static int Phi(int n)
+    {
+        int result = n;
+        for (int i = 2; i * i <= n; ++i)
+        {
+            if (n % i == 0)
+            {
+                while (n % i == 0)
+                {
+                    n /= i;
+                }
+
+                result -= result / i;
+            }
+        }
+        if (n > 1)
+        {
+            result -= result / n;
+        }
+
+        return result;
+    }
+}
--- TrueLeetCode/Program.cs
+++
@@ -1,6 +0,0 @@
-using TrueLeetCode.Leetcode.HashTable;
-
-var l = new L387();
-
-var q = l.FirstUniqChar("bbbb");
-Console.WriteLine(  q);
