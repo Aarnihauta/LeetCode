6182c2209800f656a1e15bef69e56fe70666b287
Nikita Bukov <n.bukov@smartway.today>
2023-09-09 19:52:59 +0300

    Complete L338

---
+++ TrueLeetCode/Leetcode/DP/L338.cs
@@ -0,0 +1,27 @@
+﻿namespace TrueLeetCode.Leetcode.DP;
+
+//https://leetcode.com/problems/counting-bits/description/
+public class L338
+{
+    public int[] CountBits(int n)
+    {
+        int[] res = new int[n + 1];
+        for (int i = 0; i < n + 1; i++)
+        {
+            res[i] = HammingWeight(i);
+        }
+
+        return res;
+    }
+
+    private int HammingWeight(int n)
+    {
+        int res = 0;
+        while (n != 0)
+        {
+            res += n & 1;
+            n = n >> 1;
+        }
+        return res;
+    }
+}
--- TrueLeetCode/Program.cs
+++
@@ -1,7 +0,0 @@
-using TrueLeetCode.Leetcode.Common;
-
-var q = new L204();
-
-int r = q.CountPrimes(10000);
-
-Console.WriteLine(r);
