b9f9965cfab69c1e9ca5f1344638d9906a90ecd1
Nikita Bukov <n.bukov@smartway.today>
2023-09-13 21:48:40 +0300

    Complete L1863

---
+++ TrueLeetCode/Leetcode/BitManipulations/L1863.cs
@@ -0,0 +1,33 @@
+﻿namespace TrueLeetCode.Leetcode.BitManipulations;
+
+//https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+public class L1863
+{
+    public int SubsetXORSum(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+
+        var q = nums.Where((item, idx) => ((1 << idx) & 1) != 0);
+
+        var items = Enumerable
+            .Range(1, (1 << nums.Length) - 1)
+            .Select(i => nums.Where((item, idx) => ((1 << idx) & i) != 0).ToArray())
+            .ToArray();
+
+        int res = 0;
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            int xor = 0;
+            for(int j = 0; j < items[i].Length; j++)
+            {
+                xor ^= items[i][j];
+            }
+            res += xor;
+        }
+        return res;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,4 +1,4 @@
 using TrueLeetCode.Leetcode.BitManipulations;
 
-var l = new L1486();
-l.XorOperation(5, 0);
+var l = new L1863();
+l.SubsetXORSum(new int[] { 3, 4, 5, 6, 7, 8 });
