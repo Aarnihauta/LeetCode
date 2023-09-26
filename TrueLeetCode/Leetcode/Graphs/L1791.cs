7378e018b0a0d7d6e27390db9ba75bc4138b2003
Nikita Bukov <n.bukov@smartway.today>
2023-09-26 09:38:38 +0300

    Complete L1791

---
+++ TrueLeetCode/Leetcode/Graphs/L1791.cs
@@ -0,0 +1,14 @@
+﻿namespace TrueLeetCode.Leetcode.Graphs;
+
+//https://leetcode.com/problems/find-center-of-star-graph/
+public class L1791
+{
+    public int FindCenter(int[][] edges)
+    {
+        var e1 = edges[0];
+        var e2 = edges[1];
+
+        var intersect = e1.Intersect(e2).First();
+        return intersect;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,26 +1,39 @@
-using TrueLeetCode.CSC.DataStructure;
+//using TrueLeetCode.CSC.DataStructure;
 
-var g = new Int32Graph();
+//var g = new Int32Graph();
 
-var v0 = new Vertex<int>(0, 0);
-var v1 = new Vertex<int>(1, 1);
-var v2 = new Vertex<int>(2, 2);
-var v3 = new Vertex<int>(3, 3);
-var v4 = new Vertex<int>(4, 4);
-var v5 = new Vertex<int>(5, 5);
+//var v0 = new Vertex<int>(0, 0);
+//var v1 = new Vertex<int>(1, 1);
+//var v2 = new Vertex<int>(2, 2);
+//var v3 = new Vertex<int>(3, 3);
+//var v4 = new Vertex<int>(4, 4);
+//var v5 = new Vertex<int>(5, 5);
 
-g.AddVertex(v0);
-g.AddVertex(v1);
-g.AddVertex(v2);
-g.AddVertex(v3);
-g.AddVertex(v4);
-g.AddVertex(v5);
+//g.AddVertex(v0);
+//g.AddVertex(v1);
+//g.AddVertex(v2);
+//g.AddVertex(v3);
+//g.AddVertex(v4);
+//g.AddVertex(v5);
 
-g.AddEdge(v4, v1);
-g.AddEdge(v4, v2);
-g.AddEdge(v4, v3);
-g.AddEdge(v4, v5);
+//g.AddEdge(v4, v1);
+//g.AddEdge(v4, v2);
+//g.AddEdge(v4, v3);
+//g.AddEdge(v4, v5);
 
-g.BreadthTraversal(v4);
-Console.WriteLine();
-g.DepthTraversal(v4);
+//g.BreadthTraversal(v4);
+//Console.WriteLine();
+//g.DepthTraversal(v4);
+
+
+using TrueLeetCode.Leetcode.Graphs;
+
+L1791 l = new L1791();
+var q = l.FindCenter(new int[][]
+{
+    new int[] {1,2},
+    new int[] {2,3},
+    new int[] {4,2},
+});
+
+Console.WriteLine(q);
