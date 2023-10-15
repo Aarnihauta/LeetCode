ad087dde8ffb357716a18538e0992c27d3f09e98
Nikita Bukov <n.bukov@smartway.today>
2023-10-15 19:03:39 +0300

    Complete L257

---
+++ TrueLeetCode/Leetcode/Trees/L257.cs
@@ -0,0 +1,31 @@
+﻿namespace TrueLeetCode.Leetcode.Trees;
+
+//https://leetcode.com/problems/binary-tree-paths/
+public class L257
+{
+    private List<string> _result = new List<string>();
+    public IList<string> BinaryTreePaths(TreeNode root)
+    {
+        Traverse(root, "");
+        return _result;
+    }
+
+    private void Traverse(TreeNode root, string str)
+    {
+        if (root.left == null && root.right == null)
+        {
+            _result.Add(str + root.val);
+        }
+        else
+        {
+            if (root.left != null)
+            {
+                Traverse(root.left, str + $"{root.val}->");
+            }
+            if (root.right != null)
+            {
+                Traverse(root.right, str + $"{root.val}->");
+            }
+        }
+    }
+}
