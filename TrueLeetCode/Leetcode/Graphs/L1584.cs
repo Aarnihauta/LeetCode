82c6174fb912edf466d48c00ec44496f6e6c8e88
Nikita Bukov <n.bukov@smartway.today>
2023-10-15 19:02:13 +0300

    wip L1584

--- TrueLeetCode/Leetcode/Graphs/L1584.cs
+++ TrueLeetCode/Leetcode/Graphs/L1584.cs
@@ -5,11 +5,57 @@ public class L1584
 {
     public int MinCostConnectPoints(int[][] points)
     {
-        return 1;
-        //var q = new PriorityQueue<(int, int), int>();
-        //for (int i = 0; i < points.Length; i++)
-        //{
-        //    q.
-        //}
+        int result = 0;
+        int edgesCount = points.Length * (points.Length - 1) / 2;
+        var edges = new List<(int[] From, int[] To, int Weight)>();
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            var v = points[i];
+
+            for (int j = 1; j < points.Length; j++)
+            {
+                var u = points[j];
+                int w = GetWeight(v, u);
+
+                if (!edges.Contains((u, v, w)) && v != u)
+                {
+                    edges.Add((v, u, w));
+                }
+            }
+        }
+
+        edges = edges.OrderBy(x => x.Weight).ToList();
+
+        Dictionary<int[], HashSet<int[]>> trees = new Dictionary<int[], HashSet<int[]>>();
+
+        foreach (var item in points)
+        {
+            trees.Add(item, new HashSet<int[]>() { item });
+        }
+
+        foreach (var edge in edges)
+        {
+            var from = trees[edge.From];
+            var to = trees[edge.To];
+
+            if (from != to)
+            {
+                result += edge.Weight;
+
+                foreach (var v in to)
+                {
+                    from.Add(v);
+                    trees[v] = from;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int GetWeight(int[] p1, int[] p2)
+    {
+        return Math.Abs(p1[0] - p2[0]) + Math.Abs(p1[1] - p2[1]);
     }
 }
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,32 +1,17 @@
+using TrueLeetCode.Leetcode.Graphs;
 
-using TrueLeetCode.CSC.DataStructure;
+var l = new L1584();
 
-var g = new Graph<int>();
+var a1 = new int[][]
+{
+    new [] {0,0 },new [] {2,2 },new[] {3,10 },new [] {5,2 },new[] { 7, 0 }
+};
 
-var v1 = new Vertex<int>(1, 1);
-var v2 = new Vertex<int>(2, 2);
-var v3 = new Vertex<int>(3, 3);
-var v4 = new Vertex<int>(4, 4);
-var v5 = new Vertex<int>(5, 5);
-var v6 = new Vertex<int>(6, 6);
-var v7 = new Vertex<int>(7, 7);
+var a2 = new int[][]
+{
+    new []{ -1000000, -1000000 },
+    new [] { 1000000, 1000000 }
+};
 
-g.AddVertex(v1);
-g.AddVertex(v2);
-g.AddVertex(v3);
-g.AddVertex(v4);
-g.AddVertex(v5);
-g.AddVertex(v6);
-g.AddVertex(v7);
 
-g.AddEdge(v1, v2, 28);
-g.AddEdge(v1, v6, 10);
-g.AddEdge(v2, v3, 16);
-g.AddEdge(v2, v7, 14);
-g.AddEdge(v3, v4, 12);
-g.AddEdge(v4, v7, 18);
-g.AddEdge(v4, v5, 22);
-g.AddEdge(v5, v7, 24);
-g.AddEdge(v5, v6, 25);
-
-g.GetMST();
+l.MinCostConnectPoints(a2);
