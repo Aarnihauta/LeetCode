febc2e4931bbb0cd85e79bd634040dfa19b361a5
= <shinigamichan@yandex.ru>
2023-11-21 23:58:30 +0300

    Complete L2265

--- TrueLeetCode/Leetcode/Trees/L2265.cs
+++ TrueLeetCode/Leetcode/Trees/L2265.cs
@@ -8,6 +8,52 @@ public class L2265
         var top = new Queue<TreeNode>();
         var bfs = new Queue<TreeNode>();
 
+        top.Enqueue(root);
 
+        int result = 0;
+        while(top.Any())
+        {
+            var item = top.Dequeue();
+            int sum = 0;
+            int count = 0;
+            bfs.Enqueue(item);
+
+            while (bfs.Any())
+            {
+                var curr = bfs.Dequeue();
+
+                TryEnqueueChildren(bfs, curr);
+
+                count++;
+                sum += curr.val;
+            }
+
+            if (sum / count == item.val)
+            {
+                result++;
+            }
+
+            TryEnqueueChildren(top, item);
+        }
+
+        return result;
+    }
+
+    private void TryEnqueueChildren(Queue<TreeNode> queue, TreeNode root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+
+        if (root.left != null)
+        {
+            queue.Enqueue(root.left);
+        }
+
+        if (root.right != null)
+        {
+            queue.Enqueue(root.right);
+        }
     }
 }
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,26 +1,33 @@
 using TrueLeetCode.Leetcode.Trees;
 
-var l = new L671();
 
 var root = new TreeNode
 {
-    val = 2,
+    val = 4,
     left = new TreeNode
     {
-        val = 2
+        val = 8,
+        left = new TreeNode
+        {
+            val = 0
+        },
+        right = new TreeNode
+        {
+            val = 1
+        }
     },
     right = new TreeNode
     {
         val = 5,
-        left = new TreeNode
-        {
-            val = 5
-        },
         right = new TreeNode
         {
-            val = 7
+            val = 6
         }
     }
 };
 
-l.FindSecondMinimumValue(root);
+var l = new L2265();
+
+var q = l.AverageOfSubtree(root);
+
+Console.WriteLine(q);
