63704ce4ab2b55dd4f6a6a161c95ef50754f7ee3
Nikita Bukov <n.bukov@smartway.today>
2023-11-21 21:41:33 +0300

    Complete L1389

---
+++ TrueLeetCode/Leetcode/Arrays/L1389.cs
@@ -0,0 +1,13 @@
+﻿namespace TrueLeetCode.Leetcode.Arrays;
+public class L1389
+{
+    public int[] CreateTargetArray(int[] nums, int[] index)
+    {
+        List<int> result = new List<int>();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            result.Insert(index[i], nums[i]);
+        }
+        return result.ToArray();
+    }
+}
--- TrueLeetCode/Leetcode/Arrays/L867.cs
+++ TrueLeetCode/Leetcode/Arrays/L867.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TrueLeetCode.Leetcode.Arrays;
+﻿namespace TrueLeetCode.Leetcode.Arrays;
 public class L867
 {
     public int[][] Transpose(int[][] matrix)
