196390896544b99dea8b9b2528fb01e88ed5f0a0
Aarnihauta <shinigamichan@yandex.ru>
2023-11-21 23:29:23 +0300

    wip

---
+++ TrueLeetCode/Leetcode/Trees/L2265.cs
@@ -0,0 +1,13 @@
+﻿namespace TrueLeetCode.Leetcode.Trees;
+
+//https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree/
+public class L2265
+{
+    public int AverageOfSubtree(TreeNode root)
+    {
+        var top = new Queue<TreeNode>();
+        var bfs = new Queue<TreeNode>();
+
+
+    }
+}
