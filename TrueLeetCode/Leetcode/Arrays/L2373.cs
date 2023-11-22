b3a8de5bd8b686b280e2cf16e9e65055c4708d9f
Aarnihauta <shinigamichan@yandex.ru>
2023-11-22 18:45:30 +0300

    Complete L2373

---
+++ TrueLeetCode/Leetcode/Arrays/L2373.cs
@@ -0,0 +1,45 @@
+﻿namespace TrueLeetCode.Leetcode.Arrays;
+
+//https://leetcode.com/problems/largest-local-values-in-a-matrix/
+public class L2373
+{
+    public int[][] LargestLocal(int[][] grid)
+    {
+        int n = grid.Length;
+        int k = n - 2;
+        int[][] result = new int[k][];
+
+        int offsetLeft = 0;
+        int offsetTop = 0;
+
+        for (int i = 0; i < k; i++)
+        {
+            result[i] = new int[k];
+            for (int j = 0; j < k; j++)
+            {
+                result[i][j] = GetMax(grid, offsetLeft++, offsetTop);
+            }
+
+            offsetLeft = 0;
+            offsetTop++;
+        }
+
+        return result;
+    }
+
+    private int GetMax(int[][] grid, int offsetLeft, int offsetTop)
+    {
+        int n = 3;
+        int max = 0;
+
+        for (int i = offsetTop; i < n + offsetTop; i++)
+        {
+            for (int j = offsetLeft; j < n + offsetLeft; j++)
+            {
+                max = Math.Max(max, grid[i][j]);
+            }
+        }
+
+        return max;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,13 +1,15 @@
 using TrueLeetCode.Leetcode.Arrays;
 
-var l = new L1672();
+var l = new L2373();
 
 var matrix = new int[][]
 {
-    new int[] { 2, 8, 7 },
-    new int[] { 7, 1, 3 },
-    new int[] { 1, 9, 5 },
+    new int[] { 1,1,1,1,1 },
+    new int[] { 1,1,1,1,1 },
+    new int[] { 1,1,2,1,1 },
+    new int[] { 1,1,1,1,1 },
+    new int[] { 1,1,1,1,1 },
 };
 
-var q =  l.MaximumWealth(matrix);
+var q =  l.LargestLocal(matrix);
 Console.WriteLine(q);
