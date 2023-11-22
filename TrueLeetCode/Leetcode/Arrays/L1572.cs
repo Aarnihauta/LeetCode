95392e30126c65222d3f12300141e2e412ad847d
= <shinigamichan@yandex.ru>
2023-11-22 12:29:17 +0300

    Complete L1572

---
+++ TrueLeetCode/Leetcode/Arrays/L1572.cs
@@ -0,0 +1,22 @@
+﻿namespace TrueLeetCode.Leetcode.Arrays;
+
+//https://leetcode.com/problems/matrix-diagonal-sum/
+public class L1572
+{
+    public int DiagonalSum(int[][] mat)
+    {
+        int m = mat.Length;
+        int sum = 0;
+        for (int i = 0; i < m; i++)
+        {
+            int r = m - i - 1;
+            if (r != i)
+            {
+                sum += mat[i][r];
+            }
+            sum += mat[i][i];
+        }
+
+        return sum;
+    }
+}
