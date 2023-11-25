8de5f432b0a2a742ae9c9e58db6bb8460fcb6a51
Aarnihauta <shinigamichan@yandex.ru>
2023-11-25 16:11:53 +0300

    wip

---
+++ TrueLeetCode/CSC/Collection/CollectionOperators.cs
@@ -0,0 +1,41 @@
+﻿namespace TrueLeetCode.CSC.Collection;
+public class CollectionOperators
+{
+    //A\B := {x ∈ M | (x ∈ A)∧(x ∈/ B)}
+    public IEnumerable<T> Diff<T>(IEnumerable<T> first, IEnumerable<T> second)
+    {
+        foreach (var item in first)
+        {
+            if (!second.Contains(item))
+            {
+                yield return item;
+            }
+        }
+    }
+
+    //A∪B = {x ∈ M | (x ∈ A)∨(x ∈ B)}
+    public IEnumerable<T> Concat<T>(IEnumerable<T> first, IEnumerable<T> second)
+    {
+        foreach (var item in first)
+        {
+            yield return item;
+        }
+
+        foreach (var item in second)
+        {
+            yield return item;
+        }
+    }
+
+    //A∩B := {x ∈ M | (x ∈ A)∧(x ∈ B)}
+    public IEnumerable<T> Intersection<T>(IEnumerable<T> first, IEnumerable<T> second)
+    {
+        foreach (var item in first)
+        {
+            if(second.Contains(item))
+            {
+                yield return item;
+            }
+        }
+    }
+}
---
+++ TrueLeetCode/Leetcode/Queue/L2357.cs
@@ -0,0 +1,10 @@
+﻿namespace TrueLeetCode.Leetcode.Queue;
+
+//https://leetcode.com/problems/make-array-zero-by-subtracting-equal-amounts/
+public class L2357
+{
+    public int MinimumOperations(int[] nums)
+    {
+        var queue = new PriorityQueue<int, int>();
+    }
+}
