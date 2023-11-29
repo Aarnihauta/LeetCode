1bc5440c56bad9eb9462edc38159d567d419c4dd
Aarnihauta <shinigamichan@yandex.ru>
2023-11-29 10:49:24 +0300

    Complete L191

---
+++ TrueLeetCode/Leetcode/BitManipulations/L191.cs
@@ -0,0 +1,16 @@
+﻿namespace TrueLeetCode.Leetcode.BitManipulations;
+public class L191
+{
+    public int HammingWeight(uint n)
+    {
+        uint result = 0;
+
+        while(n != 0)
+        {
+            result += n & 1;
+            n >>= 1;
+        }
+
+        return (int)result;
+    }
+}
