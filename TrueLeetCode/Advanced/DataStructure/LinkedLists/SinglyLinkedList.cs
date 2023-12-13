6ddcb9106bafdf2f760b19074ee8eb1d310e0bfb
Aarnihauta <shinigamichan@yandex.ru>
2023-12-13 20:20:12 +0300

    wip linked list

---
+++ TrueLeetCode/Advanced/DataStructure/LinkedLists/SinglyLinkedList.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace TrueLeetCode.Advanced.DataStructure.LinkedLists;
+public class SinglyLinkedList
+{
+    public SinglyLinkedList(SinglyLinkedListNode head)
+    {
+        Head = head;
+        Tail = head;
+    }
+
+    public SinglyLinkedList(int value) : this(new SinglyLinkedListNode(value))
+    {
+
+    }
+    public SinglyLinkedListNode Head { get; private set; }
+    public SinglyLinkedListNode Tail { get; private set; }
+
+    //O(1)
+    public void InsertLast(int value)
+    {
+        Tail.Next = new SinglyLinkedListNode(value);
+    }
+
+    public void Insert(SinglyLinkedListNode node, int value)
+    {
+
+    }
+
+    //O(n)
+    public void Remove(SinglyLinkedListNode node)
+    {
+        if(node == Head)
+        {
+            Head = Head.Next;
+        }
+    }
+
+    //O(n)
+    public void Draw()
+    {
+        var curr = Head;
+        StringBuilder sb = new StringBuilder();
+        while (curr != null)
+        {
+            sb.Append(curr.Value);
+            sb.Append("->");
+            curr = curr.Next;
+        }
+        sb.Remove(sb.Length - 2, 2);
+        Console.WriteLine(sb);
+    }
+}
+
+public class SinglyLinkedListNode
+{
+    public SinglyLinkedListNode(int value, SinglyLinkedListNode next = default)
+    {
+        Value = value;
+        Next = next;
+    }
+
+    public int Value { get; set; }
+    public SinglyLinkedListNode Next { get; set; }
+}
---
+++ TrueLeetCode/Program.cs
@@ -0,0 +1,10 @@
+using TrueLeetCode.Advanced.DataStructure.LinkedLists;
+
+SinglyLinkedList list = new SinglyLinkedList(0);
+
+list.InsertLast(1);
+list.InsertLast(2);
+list.InsertLast(3);
+list.Draw();
+list.Remove(list.Head.Next.Next);
+list.Draw();
