648ae27312d5bae31f9deb8323c703e4582041b9
Aarnihauta <shinigamichan@yandex.ru>
2023-12-22 16:56:08 +0300

    Complete L2656

---
+++ TrueLeetCode/Leetcode/Greedy/L2656.cs
@@ -0,0 +1,16 @@
+﻿namespace TrueLeetCode.Leetcode.Greedy;
+public class L2656
+{
+    public int MaximizeSum(int[] nums, int k)
+    {
+        int max = nums.Max();
+        int sum = 0;
+        for (int i = 0; i < k; i++)
+        {
+            sum += max;
+            max++;
+        }
+
+        return sum;
+    }
+}
