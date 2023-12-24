a8b7fb9e56e658e4784f80df42d5ca89147dddbc
Aarnihauta <shinigamichan@yandex.ru>
2023-12-24 14:32:32 +0300

    Complete L2221

---
+++ TrueLeetCode/Leetcode/Combinatorics/L2221.cs
@@ -0,0 +1,28 @@
+﻿namespace TrueLeetCode.Leetcode.Combinatorics;
+
+//https://leetcode.com/problems/find-triangular-sum-of-an-array/
+public class L2221
+{
+    public int TriangularSum(int[] nums)
+    {
+        List<int> result = new List<int>();
+        int count = nums.Length;
+        while (count > 1)
+        {
+            for (int i = 1; i < count; i++)
+            {
+                int val = nums[i] + nums[i - 1];
+                if(val >= 10)
+                {
+                    val = val % 10;
+                }
+                result.Add(val);
+            }
+            nums = result.ToArray();
+            result.Clear();
+            count--;
+        }
+
+        return nums[0];
+    }
+}
---
+++ TrueLeetCode/Program.cs
@@ -0,0 +1,7 @@
+using TrueLeetCode.Leetcode.Combinatorics;
+
+var l = new L2221();
+
+var arr = new int[] { 1, 2, 3, 4, 5 };
+
+l.TriangularSum(arr);
