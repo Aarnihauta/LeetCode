5dac0e69fe6738feca3fa4f4d1889beaaddacf05
Aarnihauta <shinigamichan@yandex.ru>
2024-02-06 09:18:08 +0300

    wip

---
+++ TrueLeetCode/Common/Strings/Searching.cs
@@ -0,0 +1,26 @@
+﻿namespace TrueLeetCode.Common.Strings;
+public class Searching
+{
+    public static int[] NaiveStringMatch(string t, string p)
+    {
+        int n = t.Length;
+        int m = p.Length;
+        List<int> result = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            int j;
+            for (j = 0; j < m; j++)
+            {
+                if (t[i + j] != p[j])
+                    break;
+            }
+
+            if (j == m)
+            {
+                result.Add(i);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
--- TrueLeetCode/Program.cs
+++
@@ -1,7 +0,0 @@
-
-
-using TrueLeetCode.Chars;
-
-var str = ImageToAscii.AsAscii(new System.Drawing.Bitmap("C:\\Users\\Nikita\\Downloads\\Telegram Desktop\\qqq.jpg"));
-
-Console.WriteLine(str);
