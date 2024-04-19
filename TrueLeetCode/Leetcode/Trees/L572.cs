e1525e211097ca46834f6dacf3b4c4ec7eb3d777
Nikita Bukov <shinigamichan@yandex.ru>
2024-04-19 11:39:57 +0300

    complete L572

---
+++ TrueLeetCode/Leetcode/Trees/L572.cs
@@ -0,0 +1,44 @@
+﻿namespace TrueLeetCode.Leetcode.Trees;
+
+//https://leetcode.com/problems/subtree-of-another-tree/
+public class L572
+{
+    public bool IsSubtree(TreeNode root, TreeNode subRoot)
+    {
+        if(subRoot == null)
+        {
+            return true;
+        }
+
+        if(root == null)
+        {
+            return false;
+        }
+
+        if(NodeEquals(root, subRoot))
+        {
+            return true;
+        }
+
+        return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+    }
+
+    private bool NodeEquals(TreeNode root, TreeNode subRoot)
+    {
+        if(root == null && subRoot == null)
+        {
+            return true;
+        }
+
+        if(
+            root != null 
+            && subRoot != null
+            && root.val == subRoot.val
+            && NodeEquals(root.left, subRoot.left) && NodeEquals(root.right, subRoot.right))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+}
