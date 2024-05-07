4633a7f481bfd1001d729051f0ac393c8c1827e8
Nikita Bukov <shinigamichan@yandex.ru>
2024-05-08 00:21:10 +0300

    complete L300

---
+++ TrueLeetCode/Leetcode/DP/L300.cs
@@ -0,0 +1,23 @@
+﻿namespace TrueLeetCode.Leetcode.DP;
+
+//https://leetcode.com/problems/longest-increasing-subsequence/
+public class L300
+{
+    public int LengthOfLIS(int[] nums)
+    {
+        int n = nums.Length;
+        int[] dp = new int[n];
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
+                {
+                    dp[i] = dp[j] + 1;                
+                }
+            }
+        }
+
+        return dp.Max() + 1;
+    }
+}
