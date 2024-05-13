39f4e7083a47535c44160cb7ad59c19cc758ed10
Nikita Bukov <shinigamichan@yandex.ru>
2024-05-13 15:06:15 +0300

    complete L53

---
+++ TrueLeetCode/Leetcode/DP/L53.cs
@@ -0,0 +1,19 @@
+﻿namespace TrueLeetCode.Leetcode.DP;
+
+//https://leetcode.com/problems/maximum-subarray/
+public class L53
+{
+    public int MaxSubArray(int[] nums)
+    {
+        int sum = int.MinValue;
+        int currentSum = 0;
+
+        foreach (var x in nums)
+        {
+            currentSum = Math.Max(x, currentSum + x);
+            sum = Math.Max(sum, currentSum);
+        }
+
+        return sum;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,5 +1,5 @@
 using TrueLeetCode.Leetcode.DP;
 
-var l = new L42();
+var l = new L53();
 
-l.Trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 });
+l.MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
