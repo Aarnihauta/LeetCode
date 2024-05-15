88fce09efca03ed95e85091c2c4c5df14de0c582
Nikita Bukov <shinigamichan@yandex.ru>
2024-05-15 22:39:29 +0300

    Complete L401

---
+++ TrueLeetCode/Leetcode/Backtracking/L401.cs
@@ -0,0 +1,23 @@
+﻿namespace TrueLeetCode.Leetcode.Backtracking;
+
+//https://leetcode.com/problems/binary-watch/
+public class L401
+{
+    public IList<string> ReadBinaryWatch(int turnedOn)
+    {
+        List<string> list = new List<string>();
+
+        foreach (var h in Enumerable.Range(0, 12))
+        {
+            foreach (var m in Enumerable.Range(0, 60))
+            {
+                if(int.PopCount(h << 6 | m) == turnedOn)
+                {
+                    list.Add($"{h}:{m:d2}");
+                }
+            }
+        }
+
+        return list;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,10 +1,5 @@
 using TrueLeetCode.Leetcode.Backtracking;
 
-var l = new L1219();
+var l = new L401();
 
-l.GetMaximumGold(new int[][]
-{
-    new int[] {0,6,0},
-    new int[] {5,8,7},
-    new int[] {0,9,0},
-});
+l.ReadBinaryWatch(8);
