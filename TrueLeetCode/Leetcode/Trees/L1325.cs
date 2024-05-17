4e953c7be050d40ad23f428b88e008ffa0363a5d
Nikita Bukov <shinigamichan@yandex.ru>
2024-05-17 17:07:07 +0300

    complete L1325

---
+++ TrueLeetCode/Leetcode/Trees/L1325.cs
@@ -0,0 +1,23 @@
+﻿namespace TrueLeetCode.Leetcode.Trees;
+
+//https://leetcode.com/problems/delete-leaves-with-a-given-value/
+public class L1325
+{
+    public TreeNode RemoveLeafNodes(TreeNode root, int target)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+
+        root.left = RemoveLeafNodes(root.left, target);
+        root.right = RemoveLeafNodes(root.right, target);
+        
+        if (root.left == null && root.right == null && root.val == target)
+        {
+            return null;
+        }
+
+        return root;
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,5 +1,17 @@
-using TrueLeetCode.Leetcode.Backtracking;
+using TrueLeetCode.Leetcode.Trees;
 
-var l = new L47();
+var l = new L1325();
 
-l.PermuteUnique(new int[] { 2, 2, 1, 1 });
+l.RemoveLeafNodes(new TreeNode
+{
+    val = 1,
+    left = new TreeNode(2)
+    {
+        left = new TreeNode(2)
+    },
+    right = new TreeNode(3)
+    {
+        left = new TreeNode(2),
+        right = new TreeNode(4)
+    }
+}, 2);
