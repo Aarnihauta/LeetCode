133bc4576dca35d337fd6b301558fa9016a1b2cd
Nikita Bukov <shinigamichan@yandex.ru>
2024-05-20 14:48:41 +0300

    complete L1863

--- TrueLeetCode/Leetcode/BitManipulations/L1863.cs
+++ TrueLeetCode/Leetcode/BitManipulations/L1863.cs
@@ -5,27 +5,16 @@ public class L1863
 {
     public int SubsetXORSum(int[] nums)
     {
-        if (nums.Length == 0)
+        return Backtrack(nums, 0, 0);
+    }
+
+    private int Backtrack(int[] nums, int idx, int val)
+    {
+        if (idx >= nums.Length)
         {
-            return 0;
+            return val;
         }
 
-        var items = Enumerable
-            .Range(1, 1 << nums.Length - 1)
-            .Select(i => nums.Where((item, idx) => (1 << idx & i) != 0).ToArray())
-            .ToArray();
-
-        int res = 0;
-
-        for (int i = 0; i < items.Length; i++)
-        {
-            int xor = 0;
-            for (int j = 0; j < items[i].Length; j++)
-            {
-                xor ^= items[i][j];
-            }
-            res += xor;
-        }
-        return res;
+        return Backtrack(nums, idx + 1, val ^ nums[idx]) + Backtrack(nums, idx + 1, val);
     }
 }
