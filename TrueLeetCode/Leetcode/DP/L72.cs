0ae33031898dfd3f3d8c6851731abfe8bc136186
Nikita Bukov <shinigamichan@yandex.ru>
2024-09-14 18:49:26 +0300

    complete L72

---
+++ TrueLeetCode/Leetcode/DP/L72.cs
@@ -0,0 +1,39 @@
+﻿namespace TrueLeetCode.Leetcode.DP;
+
+//https://leetcode.com/problems/edit-distance
+public class L72
+{
+    public int MinDistance(string word1, string word2)
+    {
+        int m = word1.Length;
+        int n = word2.Length;
+
+        int[,] dp = new int[m + 1, n + 1];
+
+        for (int i = 0; i <= m; i++)
+        {
+            dp[i, 0] = i; // Deleting all characters from word1
+        }
+        for (int j = 0; j <= n; j++)
+        {
+            dp[0, j] = j; // Inserting all characters into word1
+        }
+
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                {
+                    dp[i, j] = dp[i - 1, j - 1]; // No operation needed
+                }
+                else
+                {
+                    dp[i, j] += 1 + Math.Min(dp[i - 1, j], Math.Min(dp[i, j - 1], dp[i - 1, j - 1]));
+                }
+            }
+        }
+
+        return dp[m, n];
+    }
+}
--- TrueLeetCode/Program.cs
+++ TrueLeetCode/Program.cs
@@ -1,20 +1,5 @@
 using TrueLeetCode.Leetcode.DP;
 
-var l = new L64();
+var l = new L72();
 
-int[][] arr = new int[3][]
-{
-    new int[] { 1, 3, 1 },
-    new int[] { 1, 5, 1 },
-    new int[] { 4, 2, 1 }
-};
-
-int[][] arr2 = new int[2][]
-{
-    new int[] { 1, 2, 3 },
-    new int[] { 4, 5, 6 },
-};
-
-int result = l.MinPathSum(arr2);
-
-Console.WriteLine(result);
+l.MinDistance("horse", "ros");
